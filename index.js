361b3194b2b54b6ee0295c7e56dab47517f366db
Christian Dike <chreestiandike@gmail.com>
2022-07-20 20:45:44 +0100

    Change CORS configuration

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require("express");
 const morgan = require("morgan");
+const cors = require('cors')
 const app = express();
 
 let persons = [
@@ -49,6 +50,8 @@ const getID = () => {
 };
 
 app.use(express.json());
+
+app.use(cors())
 
 app.use(
   morgan(":method :url :status :res[content-length] :body - :response-time ms")
